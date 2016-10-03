2e00a5020da21beb85d16b2514cb96583f51b925
PvdK <pepijn.vandekamp@student.uva.nl>
2016-10-03 16:56:19 +0200

    Lab3 refactoring

--- Lab3/BoardPanel.cs
+++ Lab3/BoardPanel.cs
@@ -1,34 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using Rectangle = eu.sig.training.ch05.boardpanel.v2.Rectangle;
 
-namespace eu.sig.training.ch05.boardpanel.v1
+namespace eu.sig.training.ch05.boardpanel.v3
 {
+
     public class BoardPanel
     {
-        // tag::render[]
         /// <summary>
         /// Renders a single square on the given graphics context on the specified
         /// rectangle.
-        ///
         /// <param name="square">The square to render.</param>
         /// <param name="g">The graphics context to draw on.</param>
-        /// <param name="x">The x position to start drawing.</param>
-        /// <param name="y">The y position to start drawing.</param>
-        /// <param name="w">The width of this square (in pixels.)</param>
-        /// <param name="h">The height of this square (in pixels.)</param>
-        private void Render(Square square, Graphics g, int x, int y, int w, int h)
+        /// <param name="r">The position and dimension for rendering the square.</param>
+        private void Render(Square square, Graphics g, Rectangle r)
         {
-            square.Sprite.Draw(g, x, y, w, h);
+            Point position = r.Position;
+            square.Sprite.Draw(g, r);
             foreach (Unit unit in square.Occupants)
             {
-                unit.Sprite.Draw(g, x, y, w, h);
+                unit.Sprite.Draw(g, r);
             }
         }
-        // end::render[]
 
         private class Sprite
         {
-            public void Draw(Graphics g, int x, int y, int w, int h)
+            public void Draw(Graphics g, Rectangle r)
             {
 
             }
@@ -43,7 +40,7 @@ namespace eu.sig.training.ch05.boardpanel.v1
         {
 
             public IList<Unit> Occupants { get; set; }
-        }
 
+        }
     }
 }
