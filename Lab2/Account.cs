5fda2cfbba93f93aa71872473a4e608e894f4e7e
PvdK <pepijn.vandekamp@student.uva.nl>
2016-10-03 15:35:13 +0200

    Refactored magic numbers

--- Lab2/Account.cs
+++ Lab2/Account.cs
@@ -8,6 +8,9 @@ namespace eu.sig.training.ch04.v3
     // tag::Account[]
     public abstract class Account
     {
+        private const int ACCOUNT_NUMBER_LENGTH = 9;
+        private const int VALIDATION_MODULO = 11;
+
         public virtual Transfer MakeTransfer(string counterAccount, Money amount)
         {
             if (IsValid(counterAccount))
@@ -23,12 +26,16 @@ namespace eu.sig.training.ch04.v3
 
         public static bool IsValid(string number)
         {
+            if (String.IsNullOrEmpty(number) || number.Length != ACCOUNT_NUMBER_LENGTH)
+            {
+                return false;
+            }
             int sum = 0;
             for (int i = 0; i < number.Length; i++)
             {
-                sum = sum + (9 - i) * (int)Char.GetNumericValue(number[i]);
+                sum = sum + (ACCOUNT_NUMBER_LENGTH - i) * (int)Char.GetNumericValue(number[i]);
             }
-            return sum % 11 == 0;
+            return sum % VALIDATION_MODULO == 0;
         }
     }
     // end::Account[]
